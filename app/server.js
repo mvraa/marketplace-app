a289efe062f1759eeddedfffcb4c14ecad48ae9b
mvraa <40834090+mvraa@users.noreply.github.com>
2024-04-21 10:38:18 +0200

    template

---
+++ app/server.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bodyParse = require('body-parser');
+const livereload = require('livereload');
+const connectLiveReload = require('connect-livereload');
+const app = require('express')();
+const moment = require('moment');
+
+// Live Reload configuration
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
+// Fontend route
+const FrontRouter = require('./routes/front');
+
+// Set ejs template engine
+app.set('view engine', 'ejs');
+
+app.use(connectLiveReload())
+
+app.use(bodyParse.urlencoded({ extended: false }));
+app.locals.moment = moment;
+
+// Database connection
+const db = require('./config/keys').mongoProdURI;
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log(`Mongodb Connected`))
+    .catch(error => console.log(error));
+
+
+app.use(FrontRouter);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
